1499ad93af7b751bbc545b861a8594779bbeee41
zhuweihao <2548237631@qq.com>
2020-05-20 13:55:15 +0800

    项目初始结构创建

---
+++ warehouse/src/test/java/com/zhuweihao/mall/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuweihao.mall.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
